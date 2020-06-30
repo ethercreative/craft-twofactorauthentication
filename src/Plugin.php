ccaa5b07cb164465a95230693c68e0e2d4d1206e
James Edmonston <james@ethercreative.co.uk>
2020-06-30 15:59:51 +0100

    Fix conflict with sebastianlenz/linkfield

--- src/Plugin.php
+++ src/Plugin.php
@@ -17,6 +17,7 @@ use craft\events\RegisterUrlRulesEvent;
 use craft\events\RegisterComponentTypesEvent;
 use craft\events\RegisterElementTableAttributesEvent;
 use craft\events\SetElementTableAttributeHtmlEvent;
+use craft\services\Plugins;
 use craft\web\UrlManager;
 use craft\web\twig\variables\CraftVariable;
 use yii\base\Event;
@@ -47,83 +48,85 @@ class Plugin extends CraftPlugin
         parent::init();
         self::$plugin = $this;
 
-        $request = Craft::$app->getRequest();
-        $response = Craft::$app->getResponse();
+        Event::on(Plugins::class, Plugins::EVENT_AFTER_LOAD_PLUGINS, function (Event $event) {
+            $request = Craft::$app->getRequest();
+            $response = Craft::$app->getResponse();
 
-        if (!$this->isInstalled || $request->getIsConsoleRequest()) {
-            return;
-        }
+            if (!$this->isInstalled || $request->getIsConsoleRequest()) {
+                return;
+            }
 
-        // Register Components (Services)
-        $this->setComponents([
-            'request' => RequestService::class,
-            'response' => ResponseService::class,
-            'verify' => VerifyService::class,
-        ]);
+            // Register Components (Services)
+            $this->setComponents([
+                'request' => RequestService::class,
+                'response' => ResponseService::class,
+                'verify' => VerifyService::class,
+            ]);
 
-        $this->request->validateRequest();
+            $this->request->validateRequest();
 
-        // Verify after login.
-        Event::on(\craft\web\User::class, \craft\web\User::EVENT_AFTER_LOGIN, function (UserEvent $event) {
-            $this->request->userLoginEventHandler($event);
-        });
+            // Verify after login.
+            Event::on(\craft\web\User::class, \craft\web\User::EVENT_AFTER_LOGIN, function (UserEvent $event) {
+                $this->request->userLoginEventHandler($event);
+            });
 
-        Event::on(CraftVariable::class, CraftVariable::EVENT_INIT, function (Event $event) {
-            /** @var CraftVariable $variable */
-            $variable = $event->sender;
-            $variable->set('twoFactorAuthentication', Variables::class);
-        });
+            Event::on(CraftVariable::class, CraftVariable::EVENT_INIT, function (Event $event) {
+                /** @var CraftVariable $variable */
+                $variable = $event->sender;
+                $variable->set('twoFactorAuthentication', Variables::class);
+            });
 
-        Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_CP_URL_RULES, function (RegisterUrlRulesEvent $event) {
-            $event->rules['two-factor-authentication'] = 'two-factor-authentication/settings/index';
-        });
+            Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_CP_URL_RULES, function (RegisterUrlRulesEvent $event) {
+                $event->rules['two-factor-authentication'] = 'two-factor-authentication/settings/index';
+            });
 
-        // Register our widgets
-        Event::on(Dashboard::class, Dashboard::EVENT_REGISTER_WIDGET_TYPES, function (RegisterComponentTypesEvent $event) {
-            $event->types[] = NotifyWidget::class;
-        });
+            // Register our widgets
+            Event::on(Dashboard::class, Dashboard::EVENT_REGISTER_WIDGET_TYPES, function (RegisterComponentTypesEvent $event) {
+                $event->types[] = NotifyWidget::class;
+            });
 
-        /**
-         * Adds the following attributes to the User table in the CMS
-         * NOTE: You still need to select them with the 'gear'
-         */
-        Event::on(User::class, Element::EVENT_REGISTER_TABLE_ATTRIBUTES, function (RegisterElementTableAttributesEvent $event) {
-            $event->tableAttributes['hasTwoFactorAuthentication'] = ['label' => Craft::t('two-factor-authentication', '2-Factor Auth')];
-        });
+            /**
+             * Adds the following attributes to the User table in the CMS
+             * NOTE: You still need to select them with the 'gear'
+             */
+            Event::on(User::class, Element::EVENT_REGISTER_TABLE_ATTRIBUTES, function (RegisterElementTableAttributesEvent $event) {
+                $event->tableAttributes['hasTwoFactorAuthentication'] = ['label' => Craft::t('two-factor-authentication', '2-Factor Auth')];
+            });
 
-        /**
-         * Returns the content for the additional attributes field
-         */
-        Event::on(User::class, Element::EVENT_SET_TABLE_ATTRIBUTE_HTML, function (SetElementTableAttributeHtmlEvent $event) {
-            $currentUser = Craft::$app->getUser()->getIdentity();
-            if ($event->attribute == 'hasTwoFactorAuthentication' && $currentUser->admin) {
-                /** @var User $user */
-                $user = $event->sender;
+            /**
+             * Returns the content for the additional attributes field
+             */
+            Event::on(User::class, Element::EVENT_SET_TABLE_ATTRIBUTE_HTML, function (SetElementTableAttributeHtmlEvent $event) {
+                $currentUser = Craft::$app->getUser()->getIdentity();
+                if ($event->attribute == 'hasTwoFactorAuthentication' && $currentUser->admin) {
+                    /** @var User $user */
+                    $user = $event->sender;
 
-                if (Plugin::$plugin->verify->isEnabled($user)) {
-                    $event->html = '<div class="status enabled" title="' . Craft::t('two-factor-authentication', 'Enabled') . '"></div>';
-                } else {
-                    $event->html = '<div class="status" title="' . Craft::t('two-factor-authentication', 'Not enabled') . '"></div>';
+                    if (Plugin::$plugin->verify->isEnabled($user)) {
+                        $event->html = '<div class="status enabled" title="' . Craft::t('two-factor-authentication', 'Enabled') . '"></div>';
+                    } else {
+                        $event->html = '<div class="status" title="' . Craft::t('two-factor-authentication', 'Not enabled') . '"></div>';
+                    }
+
+                    // Prevent other event listeners from getting invoked
+                    $event->handled = true;
                 }
+            });
 
-                // Prevent other event listeners from getting invoked
-                $event->handled = true;
-            }
-        });
-        
-        /**
-         * Hook into the users cp page.
-         */
-        Craft::$app->view->hook('cp.users.edit.details', function (array &$context) {
-            if (Craft::$app->getUser()->getIsAdmin() && !$context['isNewUser']) {
-                /** @var User $user */
-                $user = $context['user'];
+            /**
+             * Hook into the users cp page.
+             */
+            Craft::$app->view->hook('cp.users.edit.details', function (array &$context) {
+                if (Craft::$app->getUser()->getIsAdmin() && !$context['isNewUser']) {
+                    /** @var User $user */
+                    $user = $context['user'];
 
-                return Craft::$app->getView()->renderTemplate('two-factor-authentication/_user/status', [
-                    'user' => $user,
-                    'enabled' => Plugin::$plugin->verify->isEnabled($user),
-                ]);
-            }
+                    return Craft::$app->getView()->renderTemplate('two-factor-authentication/_user/status', [
+                        'user' => $user,
+                        'enabled' => Plugin::$plugin->verify->isEnabled($user),
+                    ]);
+                }
+            });
         });
     }
 
